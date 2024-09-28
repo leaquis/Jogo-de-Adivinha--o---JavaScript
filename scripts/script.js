19125ef76caae29c081bec1393ff839efc02cba7
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-09-28 00:17:50 -0300

    programando as dificuldades

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,34 @@
+const guessSection = document.querySelector("guess-section");
+const guessInput = document.querySelector("#guess");
+const guessButton = document.querySelector("#guess-btn");
+const resultParagraph = document.querySelector("#result");
+const difficultySelect = document.querySelector("#difficulty");
+const difficultySection = document.querySelector("#difficulty-section");
+const gameSection = document.querySelector("#game-section");
+const triesLeftSpan = document.querySelector("#tries-left");
+const resetButton = document.querySelector("#reset-btn");
 
+let maxTries;
+let randomNumber;
+let triesLeft;
+
+difficultySelect.addEventListener("change", function() {
+    const difficulty = parseInt(difficultySelect.value);
+
+    switch (difficulty) {
+        case 1:
+            maxTries = 10;
+            break;
+        case 2:
+            maxTries = 7;
+            break;
+        case 3:
+            maxTries = 5;
+            break;
+        default:
+            maxTries = 10;
+            break;
+    }
+
+    console.log(maxTries);
+})
